4cd89d9a702e2c59816615c12539254a96f1f3bb
OliAltF4 <oliver.k06@web.de>
2024-11-20 11:19:37 +0100

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM python:3.9-slim
-
+LABEL org.opencontainers.image.source=https://github.com/OliAltF4/Wasserpegel-Rhein-Bonn
 WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY app.py .
-
-CMD ["python", "app.py"]
+# the "-u" paramenter is needed to display the log messages to stdout with the print command
+CMD ["python","-u", "app.py"] 
--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 # API URLs und Bot Token
 API_URL_CURRENT = "https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/593647aa-9fea-43ec-a7d6-6476a76ae868/W/currentmeasurement.json"
-API_URL_HISTORIC = "https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/593647aa-9fea-43ec-a7d6-6476a76ae868/W/measurements.json"
+API_URL_HISTORIC = "https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/593647aa-9fea-43ec-a7d6-6476a76ae868/W/measurements.json?start=P5D"
 #TOKEN = ''
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 # Funktion für den aktuellen Wasserstand
 def get_current_water_level():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{current_time} - received request for current water level")
     response = requests.get(API_URL_CURRENT)
     if response.status_code == 200:
         data = response.json()
@@ -20,6 +22,8 @@ def get_current_water_level():
 
 # Funktion für den maximalen Wasserstand der letzten 5 Tage
 def get_max_water_level_last_5_days():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{current_time} - received request for weekly high water level")
     end_date = datetime.now()
     start_date = end_date - timedelta(days=5)
     params = {
@@ -50,6 +54,8 @@ async def weekly(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Bot-Einrichtung
 def main():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{current_time} - Starting Aquaman Bot")
     app = Application.builder().token(TOKEN).build()
 
     # Befehle registrieren
